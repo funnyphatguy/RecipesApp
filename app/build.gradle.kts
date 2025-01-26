cb613cc61918141e814a71f58f2f4310caf2f825
funnyphatguy <contius32@gmail.com>
2025-01-26 19:47:13 +0300

    RA-00 made a new icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.funny_phat_guy.recipesapp"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
