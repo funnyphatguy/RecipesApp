4a32e5deb646692e405a04a0efbe97557dff5edc
funnyphatguy <contius32@gmail.com>
2025-03-05 01:12:41 +0300

    RA-20-01 bug fix

--- app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,18 +1,14 @@
 package ru.funny_phat_guy.recipesapp.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
-import ru.funny_phat_guy.recipesapp.model.Ingredient
+import ru.funny_phat_guy.recipesapp.model.Recipe
 
 class RecipeViewModel : ViewModel() {
 
     data class RecipeState(
-        val imageUrl: String? = null,
-        val favourites:Boolean = false,
-        val title: String? = null,
-        val seekbarCount:Int? = null,
-        val ingredients: List<Ingredient> = emptyList(),
-        val method: List<String> = emptyList(),
+        val recipe: Recipe? = null,
+        val favourites: Boolean = false,
+        val portionsCount: Int? = 1,
     ) {
     }
-
 }
