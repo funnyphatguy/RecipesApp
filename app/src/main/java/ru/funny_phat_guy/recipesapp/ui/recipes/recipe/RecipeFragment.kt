fbc85e8fae91fa4cf2014a5e6c70b8eeac27f75b
funnyphatguy <contius32@gmail.com>
2025-03-06 22:40:58 +0300

    RA-21 first commit

--- app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeFragment.kt
+++ app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeFragment.kt
@@ -4,25 +4,29 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import ru.funny_phat_guy.recipesapp.R
-import ru.funny_phat_guy.recipesapp.databinding.FragmentRecipeBinding
 import ru.funny_phat_guy.recipesapp.data.AssetsImageLoader
+import ru.funny_phat_guy.recipesapp.databinding.FragmentRecipeBinding
+import ru.funny_phat_guy.recipesapp.model.Recipe
 import ru.funny_phat_guy.recipesapp.ui.Constants.ARG_PREFERENCES
 import ru.funny_phat_guy.recipesapp.ui.Constants.ARG_RECIPE
 import ru.funny_phat_guy.recipesapp.ui.Constants.FAVORITES
-import ru.funny_phat_guy.recipesapp.model.Recipe
 
 class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = requireNotNull(_binding) { "Binding for FragmentRecipeBinding must not be null" }
+
+    private val recipeViewModel: RecipeViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -36,6 +40,15 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        recipeViewModel.recipeLiveData.observe(viewLifecycleOwner) { state ->
+            state.let {
+                Log.d(
+                    "!!!",
+                    "isFavorite: ${it.favourites}"
+                )
+            }
+        }
 
         val recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             arguments?.getParcelable(ARG_RECIPE, Recipe::class.java)
--- app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,5 +1,8 @@
 package ru.funny_phat_guy.recipesapp.ui.recipes.recipe
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.funny_phat_guy.recipesapp.model.Recipe
 
@@ -10,5 +13,17 @@ class RecipeViewModel : ViewModel() {
         val favourites: Boolean = false,
         val portionsCount: Int? = 1,
     ) {
+    }
+
+    private val _recipeLiveData = MutableLiveData<RecipeState>(RecipeState())
+
+    val recipeLiveData: LiveData<RecipeState> get() = _recipeLiveData
+
+
+    init {
+        Log.i("!!!", "Info from VM")
+
+        _recipeLiveData.value = RecipeState(favourites = true)
+
     }
 }
