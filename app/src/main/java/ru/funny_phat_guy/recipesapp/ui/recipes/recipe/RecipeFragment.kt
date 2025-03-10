0992afc2a48ea746e327cc27ae962f258baa1d5f
funnyphatguy <contius32@gmail.com>
2025-03-10 21:06:12 +0300

    RA-23-07 in progress

--- app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeFragment.kt
+++ app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeFragment.kt
@@ -1,10 +1,7 @@
 package ru.funny_phat_guy.recipesapp.ui.recipes.recipe
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,12 +13,9 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import ru.funny_phat_guy.recipesapp.R
 import ru.funny_phat_guy.recipesapp.data.AssetsImageLoader
+import ru.funny_phat_guy.recipesapp.data.STUB
 import ru.funny_phat_guy.recipesapp.databinding.FragmentRecipeBinding
-import ru.funny_phat_guy.recipesapp.model.Recipe
-import ru.funny_phat_guy.recipesapp.ui.Constants
-import ru.funny_phat_guy.recipesapp.ui.Constants.ARG_PREFERENCES
 import ru.funny_phat_guy.recipesapp.ui.Constants.ARG_RECIPE_ID
-import ru.funny_phat_guy.recipesapp.ui.Constants.FAVORITES
 
 class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
@@ -41,21 +35,13 @@ class RecipeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        arguments?.getInt(ARG_RECIPE_ID)?.let { recipeViewModel.loadRecipe(it) }
-
-        recipeViewModel.recipeLiveData.observe(viewLifecycleOwner) { state ->
-            state.let {
-                Log.i(
-                    Constants.LOG_INFO_TAG,
-                    "isFavorite: ${it.isFavourites}"
-                )
-            }
+        val id = arguments?.getInt(ARG_RECIPE_ID)
+        if (id != null) {
+            recipeViewModel.loadRecipe(id)
+            initRecycler(id)
         }
 
-
         initUI()
-
-//        initRecycler(recipeId)
 
         initDivider()
 
@@ -66,42 +52,37 @@ class RecipeFragment : Fragment() {
         _binding = null
     }
 
-    val sharedPref: SharedPreferences by lazy {
-        requireContext().getSharedPreferences(ARG_PREFERENCES, Context.MODE_PRIVATE)
-    }
-
-    private fun saveFavorites(ides: Set<String>) {
-        sharedPref.edit().putStringSet(FAVORITES, ides).apply()
-    }
 
     private fun initUI() {
 
-        binding.tvPortion.text = getString(R.string.portion_start)
+        with(binding) {
+            ivPreferences.setOnClickListener {
+                recipeViewModel.onFavoritesClicked()
+            }
+            tvPortion.text = getString(R.string.portion_start)
+        }
 
-        recipeViewModel.recipeLiveData.observe(viewLifecycleOwner) { state ->
-            state.let {
-                binding.tvRecipe.text = state.recipe?.title
+        recipeViewModel.recipeState.observe(viewLifecycleOwner) { state ->
+            with(binding) {
+                tvRecipe.text = state.recipe?.title
                 val drawableTitle = state.recipe?.imageUrl.let {
                     AssetsImageLoader.loadImage(
                         it.toString(),
                         context
                     )
                 }
-                binding.ivRecipe.setImageDrawable(drawableTitle)
+                ivRecipe.setImageDrawable(drawableTitle)
 
-                binding.ivPreferences.setImageResource(
+                ivPreferences.setImageResource(
                     if (state.isFavourites) R.drawable.ic_heart
                     else R.drawable.ic_heart_empty
                 )
-
-                binding.ivPreferences.setOnClickListener {
-                    recipeViewModel.onFavoritesClicked()
-                }
             }
         }
     }
 
-    private fun initRecycler(recipe: Recipe?) {
+    private fun initRecycler(id: Int) {
+        val recipe = STUB.getRecipeById(id)
         val ingredients = recipe?.ingredients ?: run {
             Toast.makeText(context, "Ingredient not found", Toast.LENGTH_SHORT).show()
             return
--- app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/ru/funny_phat_guy/recipesapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -4,36 +4,34 @@ package ru.funny_phat_guy.recipesapp.ui.recipes.recipe
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import ru.funny_phat_guy.recipesapp.R
 import ru.funny_phat_guy.recipesapp.data.STUB
 import ru.funny_phat_guy.recipesapp.model.Recipe
-import ru.funny_phat_guy.recipesapp.ui.Constants
 import ru.funny_phat_guy.recipesapp.ui.Constants.ARG_PREFERENCES
 import ru.funny_phat_guy.recipesapp.ui.Constants.FAVORITES
 
 class RecipeViewModel(application: Application) : AndroidViewModel(application) {
 
-    val context = getApplication<Application>().applicationContext
+    val context: Context
+        get() = getApplication<Application>().applicationContext
 
     private val sharedPref: SharedPreferences by lazy {
         context.getSharedPreferences(ARG_PREFERENCES, Context.MODE_PRIVATE)
     }
 
-    data class recipeState(
+    data class RecipeState(
         val recipe: Recipe? = null,
         val isFavourites: Boolean = false,
         val portionsCount: Int = 1,
     )
 
-    private val _recipeLiveData = MutableLiveData(recipeState())
+    private val _recipeState = MutableLiveData(RecipeState())
 
-    val recipeLiveData: LiveData<recipeState> get() = _recipeLiveData
+    val recipeState: LiveData<RecipeState> get() = _recipeState
 
-     private fun getFavorites(): HashSet<String> {
+    private fun getFavorites(): HashSet<String> {
         val favoriteSet = sharedPref.getStringSet(FAVORITES, emptySet()).orEmpty()
         return HashSet(favoriteSet)
     }
@@ -42,8 +40,8 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
         sharedPref.edit().putStringSet(FAVORITES, ides).apply()
     }
 
-    fun onFavoritesClicked(){
-        val currentState = _recipeLiveData.value ?: return
+    fun onFavoritesClicked() {
+        val currentState = _recipeState.value ?: return
         val recipeId = currentState.recipe?.id?.toString() ?: return
 
         val newFavorites = getFavorites().toMutableSet().apply {
@@ -51,16 +49,16 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
         }
 
         saveFavorites(newFavorites)
-        _recipeLiveData.value = currentState.copy(isFavourites = !currentState.isFavourites)
-        }
+        _recipeState.value = currentState.copy(isFavourites = !currentState.isFavourites)
+    }
 
     fun loadRecipe(recipeId: Int) {
         //  TODO: Load from network
-        val currentState = _recipeLiveData.value
+        val currentState = _recipeState.value
         val recipe = STUB.getRecipeById(recipeId)
         val isFavorite = recipe?.id.toString() in getFavorites()
 
-        _recipeLiveData.value = currentState?.copy(
+        _recipeState.value = currentState?.copy(
             recipe = recipe,
             isFavourites = isFavorite,
             portionsCount = currentState.portionsCount
