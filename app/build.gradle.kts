3e0073b4f337426b4022a19b3b7012e0de471442
funnyphatguy <contius32@gmail.com>
2025-04-11 00:26:19 +0300

    RA-38-02 hotfix№2: ksp version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("kotlin-parcelize")
     id("androidx.navigation.safeargs.kotlin")
     kotlin("plugin.serialization")
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20"
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0"
 }
 
 android {
@@ -43,8 +43,8 @@ android {
 }
 
 dependencies {
-    val room_version = "2.7.0"
-    implementation("androidx.room:room-runtime:$room_version")
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
     implementation (libs.glide)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
     implementation(libs.kotlinx.serialization.json)
